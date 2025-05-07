6380d3f053607ab3e2410b18c16f53ce575f058f
Frans <frans_421_12@hotmail.com>
2025-05-07 01:49:51 -0500

    build: install and config jest, script config lint, test and format

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/__tests__/**/*.test.ts'],
+    moduleFileExtensions: ['ts', 'js', 'json'],
+    transform: {
+        '^.+\\.ts$': 'ts-jest',
+    },
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov'],
+    verbose: true,
+};
